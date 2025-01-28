280697a882538ae2c2431fad4afba800758ae5da
Adam Bennett <adam@hozah.com>
2025-01-28 15:35:27 +0000

    Cloud SQL module
    Signed-off-by: Adam Bennett <adam@hozah.com>

--- cloud-sql-permissions/main.tf
+++ cloud-sql-permissions/main.tf
@@ -13,15 +13,26 @@ terraform {
 }
 
 resource "google_sql_user" "app_user" {
-  name     = trimsuffix(var.service_account_email, ".gserviceaccount.com")
+  name = trimsuffix(var.service_account_email, ".gserviceaccount.com")
   instance = reverse(split(":", var.instance_connection_name))[0]
   type     = "CLOUD_IAM_SERVICE_ACCOUNT"
 }
 
 locals {
-  code_hash = sha1(join("", [
-    for f in fileset("${path.module}/scripts/grant_sql_access", "*") : filesha1("${path.module}/scripts/grant_sql_access/${f}")
-  ], var.extra_commands))
+  script_path = "${path.module}/scripts/grant_sql_access"
+  code_hash = sha1(
+    join("",
+      [for f in fileset(local.script_path, "*") : filesha1("${local.script_path}/${f}")],
+      [
+        var.instance_connection_name,
+        var.user_name,
+        var.user_password,
+        var.database_name,
+        var.service_account_email,
+      ]
+      var.extra_commands,
+    )
+  )
 }
 
 resource "null_resource" "run_grant_sql_access" {
@@ -30,15 +41,15 @@ resource "null_resource" "run_grant_sql_access" {
   }
 
   provisioner "local-exec" {
-    working_dir = "${path.module}/scripts/grant_sql_access"
-    command     = "npm run exec"
+    working_dir = local.script_path
+    command     = "npm install && npm run exec"
     environment = {
       INSTANCE_CONNECTION_NAME = var.instance_connection_name
       USER_NAME                = var.user_name
       USER_PASSWORD            = var.user_password
       DATABASE_NAME            = var.database_name
       APP_USERNAME             = google_sql_user.app_user.name
-      EXTRA_COMMANDS           = join("", var.extra_commands)
+      EXTRA_COMMANDS = join("", var.extra_commands)
     }
   }
 }
--- cloud-sql-permissions/outputs.tf
+++ cloud-sql-permissions/outputs.tf
@@ -1,4 +1,4 @@
 output "db_user_name" {
-  description = "ID of the vpc"
+  description = "ID of the database user"
   value       = google_sql_user.app_user.name
 }
