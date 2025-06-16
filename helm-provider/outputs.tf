5e2098424e3e67d512d0068e6846744a7c80944e
Adam Bennett <adam@hozah.com>
2025-06-16 15:18:58 +0100

    Refactor outputs to group Kubernetes and registry credentials
    Signed-off-by: Adam Bennett <adam@hozah.com>

--- helm-provider/outputs.tf
+++ helm-provider/outputs.tf
@@ -1,9 +1,15 @@
-output "token" {
-  value = data.google_client_config.default.access_token
+output "kubernetes" {
+  value = {
+    token                  = data.google_client_config.default.access_token
+    host                   = "https://${data.google_container_cluster.cluster.endpoint}"
+    cluster_ca_certificate = base64decode(data.google_container_cluster.cluster.master_auth[0].cluster_ca_certificate)
+  }
 }
-output "host" {
-  value = "https://${data.google_container_cluster.cluster.endpoint}"
-}
-output "cluster_ca_certificate" {
-  value = base64decode(data.google_container_cluster.cluster.master_auth[0].cluster_ca_certificate)
+
+output "hozah_registry" {
+  value = {
+    url      = "oci://europe-docker.pkg.dev"
+    username = "oauth2accesstoken"
+    password = data.google_client_config.default.access_token
+  }
 }
