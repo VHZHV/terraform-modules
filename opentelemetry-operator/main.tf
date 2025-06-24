ec234a8d2334d66bf04908ef9992657b80f40365
Adam Bennett <54963794+adben002@users.noreply.github.com>
2025-06-24 11:20:54 +0100

    Update main.tf

--- opentelemetry-operator/main.tf
+++ opentelemetry-operator/main.tf
@@ -2,7 +2,7 @@ resource "helm_release" "k8s_operator_for_opentelemetry" {
   name             = "opentelemetry-operator"
   repository       = "https://open-telemetry.github.io/opentelemetry-helm-charts"
   chart            = "opentelemetry-operator"
-  version          = "0.90.3"
+  version          = "0.90.4"
   create_namespace = true
   set = [{
     name  = "manager.collectorImage.repository"
