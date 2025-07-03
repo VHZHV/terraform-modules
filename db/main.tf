4aacd2db8d13a14b51f164e35dbb41eef3bfaef5
Adam Bennett <adam@hozah.com>
2025-07-03 22:27:25 +0100

    Refactor main.tf to remove data source for SQL instance and use variable directly
    Signed-off-by: Adam Bennett <adam@hozah.com>

--- db/main.tf
+++ db/main.tf
@@ -1,8 +1,3 @@
-data "google_sql_database_instance" "console" {
-  name    = var.cloud_sql_instance_name
-  project = var.project_id
-}
-
 resource "random_shuffle" "password_length" {
   input        = range(30, 33) # Matches with ../sql-postgres/main.tf#password_validation_policy_config.min_length
   result_count = 1
@@ -20,14 +15,14 @@ resource "random_password" "admin_user_password" {
 
 resource "google_sql_user" "admin_user" {
   name        = "${var.database_name}-admin-user"
-  instance    = data.google_sql_database_instance.console.name
+  instance    = var.cloud_sql_instance_name
   password_wo = random_password.admin_user_password.result
 }
 
 resource "google_sql_database" "main_database" {
   project  = var.project_id
   name     = var.database_name
-  instance = data.google_sql_database_instance.console.name
+  instance = var.cloud_sql_instance_name
 
   deletion_policy = var.deletable == "" ? "ABANDON" : "DELETE"
 }
