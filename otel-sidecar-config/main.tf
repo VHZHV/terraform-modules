8f739214ce9bf9e6f085f60c223934fdae310c19
Adam Bennett <54963794+adben002@users.noreply.github.com>
2025-07-05 20:09:44 +0100

    Refactor Cloud SQL module to use new resources and update README documentation (#27)

--- otel-sidecar-config/main.tf
+++ otel-sidecar-config/main.tf
@@ -2,7 +2,7 @@ resource "helm_release" "otel-sidecar-config" {
   name             = "otel-sidecar-config"
   repository       = "oci://europe-docker.pkg.dev"
   chart            = "hozah-artifacts/docker/hozah/otel-sidecar-config"
-  version          = "1.1.0"
+  version          = "1.2.0"
   namespace        = var.k8s_namespace
   create_namespace = true
 }
