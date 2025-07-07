6e4fbd77acbec897f9c951391e5e0adf1ebe68e2
Adam Bennett <adam@hozah.com>
2025-07-07 16:08:22 +0100

    Rename output to ip_names and simplify mapping for DNS names
    Signed-off-by: Adam Bennett <adam@hozah.com>

--- ip_addresses/outputs.tf
+++ ip_addresses/outputs.tf
@@ -1,8 +1,5 @@
-output "details" {
+output "ip_names" {
   value = {
-    for index, name in var.dns_names : name => {
-      domain = module.ip_addresses.dns_fqdns[index]
-      ipName = module.ip_addresses.names[index]
-    }
+    for index, name in var.dns_names : name => module.ip_addresses.names[index]
   }
 }
