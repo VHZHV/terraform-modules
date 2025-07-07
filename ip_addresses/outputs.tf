13826c7bf4165b6944195fac3075b3c130f085d1
Adam Bennett <adam@hozah.com>
2025-07-07 14:41:30 +0100

    Fix output mapping for DNS names in outputs.tf
    Signed-off-by: Adam Bennett <adam@hozah.com>

--- ip_addresses/outputs.tf
+++ ip_addresses/outputs.tf
@@ -2,7 +2,7 @@ output "details" {
   value = {
     for index, name in var.dns_names : name => {
       domain = module.ip_addresses.dns_fqdns[index]
-      ipName = module.ip_addresses.names[name]
+      ipName = module.ip_addresses.names[index]
     }
   }
 }
