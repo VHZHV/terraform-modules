61a26eb37a9cebe5c90fe0da9e96c4bbe898ed4d
Adam Bennett <adam@hozah.com>
2025-09-13 11:18:06 +0100

    Add password_wo_version to google_sql_user resource in main.tf
    Signed-off-by: Adam Bennett <adam@hozah.com>

--- db/main.tf
+++ db/main.tf
@@ -14,9 +14,10 @@ resource "random_password" "admin_user_password" {
 }
 
 resource "google_sql_user" "admin_user" {
-  name        = "${var.database_name}-admin-user"
-  instance    = var.cloud_sql_instance_name
-  password_wo = random_password.admin_user_password.result
+  name                = "${var.database_name}-admin-user"
+  instance            = var.cloud_sql_instance_name
+  password_wo         = random_password.admin_user_password.result
+  password_wo_version = 1
 }
 
 resource "google_sql_database" "main_database" {
