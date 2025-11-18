75ef9de7c72973968828084c045e9dae9c4f5ce0
Adam Bennett <adam@hozah.com>
2025-11-18 15:06:45 +0000

    Update opentelemetry-operator chart version to 0.99.1
    Signed-off-by: Adam Bennett <adam@hozah.com>

--- opentelemetry-operator/main.tf
+++ opentelemetry-operator/main.tf
@@ -2,7 +2,7 @@ resource "helm_release" "k8s_operator_for_opentelemetry" {
   name             = "opentelemetry-operator"
   repository       = "https://open-telemetry.github.io/opentelemetry-helm-charts"
   chart            = "opentelemetry-operator"
-  version          = "0.99.0"
+  version          = "0.99.1"
   create_namespace = true
   set = [{
     name  = "manager.collectorImage.repository"
