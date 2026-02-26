255786501f3fa31b7ee36524c6861f4d4098dcc3
Adam Bennett <adam@hozah.com>
2026-02-26 22:17:10 +0000

    Update main.tf to modify custom metrics stackdriver adapter configuration and add IAM bindings for service accounts
    Signed-off-by: Adam Bennett <adam@hozah.com>

--- custom_metrics_stackdriver_adapter/main.tf
+++ custom_metrics_stackdriver_adapter/main.tf
@@ -1,19 +1,46 @@
 resource "helm_release" "custom_metrics_stackdriver_adapter" {
-  name             = "custom-metrics-stackdriver-adapter"
-  repository       = "oci://europe-docker.pkg.dev"
-  chart            = "hozah-artifacts/docker/hozah/custom-metrics-stackdriver-adapter"
-  version          = "1.14.0"
-  create_namespace = true
+  name       = "custom-metrics-stackdriver-adapter"
+  repository = "oci://europe-docker.pkg.dev"
+  chart      = "hozah-artifacts/docker/hozah/custom-metrics-stackdriver-adapter"
+  version    = "0.14.3"
+
+  set = [
+    {
+      name  = "gcpServiceAccountEmail"
+      value = google_service_account.custom_metrics.email
+    },
+  ]
 }
 
-data "google_project" "project" {
-  project_id = var.project_id
+resource "google_service_account" "custom_metrics" {
+  project    = var.project_id
+  account_id = "custom-metrics"
 }
 
-resource "google_project_iam_member" "stackdriver_monitoring_viewer" {
-  depends_on = [helm_release.custom_metrics_stackdriver_adapter]
+module "custom_metrics_project-iam-bindings" {
+  source  = "terraform-google-modules/iam/google//modules/projects_iam"
+  version = "8.2.0"
 
-  project = var.project_id
-  role    = "roles/monitoring.viewer"
-  member  = "principal://iam.googleapis.com/projects/${data.google_project.project.number}/locations/global/workloadIdentityPools/${data.google_project.project.project_id}.svc.id.goog/subject/ns/custom-metrics/sa/custom-metrics-stackdriver-adapter"
+  projects = [var.project_id]
+  mode     = "additive"
+
+  bindings = {
+    "roles/monitoring.viewer" = [
+      "serviceAccount:${google_service_account.custom_metrics.email}",
+    ]
+  }
+}
+
+module "custom_metrics_service_account-iam-bindings" {
+  source  = "terraform-google-modules/iam/google//modules/service_accounts_iam"
+  version = "8.2.0"
+
+  service_accounts = [google_service_account.custom_metrics.email]
+  project          = var.project_id
+  mode             = "additive"
+  bindings = {
+    "roles/iam.workloadIdentityUser" = [
+      "serviceAccount:${var.project_id}.svc.id.goog[custom-metrics/custom-metrics-stackdriver-adapter]"
+    ]
+  }
 }
