16ee00bba592131f7bfc1b0053b441a03d203323
Adam Bennett <adam@hozah.com>
2026-02-27 14:05:28 +0000

    Bump custom-metrics-stackdriver-adapter chart version to 0.14.6
    Signed-off-by: Adam Bennett <adam@hozah.com>

--- cert-manager/main.tf
+++ cert-manager/main.tf
@@ -2,7 +2,7 @@ resource "helm_release" "cert_manager" {
   name             = "cert-manager"
   repository       = "https://charts.jetstack.io"
   chart            = "cert-manager"
-  version          = "v1.19.3"
+  version          = "v1.19.4"
   create_namespace = true
   set = [{
     name  = "crds.enabled"
