4de430ea7f305d98f94b9af90ce5e67c5204f765
Adam Bennett <adam@hozah.com>
2026-06-15 14:52:18 +0100

    (chore) Bump deps not managed by dependabot
    Signed-off-by: Adam Bennett <adam@hozah.com>

--- opentelemetry-operator/main.tf
+++ opentelemetry-operator/main.tf
@@ -2,7 +2,7 @@ resource "helm_release" "k8s_operator_for_opentelemetry" {
   name             = "opentelemetry-operator"
   repository       = "https://open-telemetry.github.io/opentelemetry-helm-charts"
   chart            = "opentelemetry-operator"
-  version          = "0.114.1"
+  version          = "0.115.0"
   create_namespace = true
   # Need the contrib distribution for googleclientauth / otlphttp.
   set = [{
