200e9c931a5ab818f89c6fa4dafff89eb6058355
Adam Bennett <adam@hozah.com>
2026-06-24 17:32:24 +0100

    [HPS-761] Cloud Armor security policy on external load balancer
    Signed-off-by: Adam Bennett <adam@hozah.com>

--- cloud-armor/main.tf
+++ cloud-armor/main.tf
@@ -1,16 +1,14 @@
 locals {
   waf_rules = [
-    "sqli-v422-stable",
-    "xss-v422-stable",
-    "lfi-v422-stable",
-    "rfi-v422-stable",
-    "rce-v422-stable",
-    "methodenforcement-v422-stable",
-    "scannerdetection-v422-stable",
-    "protocolattack-v422-stable",
-    "sessionfixation-v422-stable",
-    "java-v422-stable",
-    "generic-v422-stable",
+    { target_rule_set = "xss-v422-stable", sensitivity_level = 4 },
+    { target_rule_set = "lfi-v422-stable", sensitivity_level = 4 },
+    { target_rule_set = "rfi-v422-stable", sensitivity_level = 4 },
+    { target_rule_set = "rce-v422-stable", sensitivity_level = 1 },
+    { target_rule_set = "scannerdetection-v422-stable", sensitivity_level = 4 },
+    { target_rule_set = "protocolattack-v422-stable", sensitivity_level = 4 },
+    { target_rule_set = "sessionfixation-v422-stable", sensitivity_level = 4 },
+    { target_rule_set = "java-v422-stable", sensitivity_level = 4 },
+    { target_rule_set = "generic-v422-stable", sensitivity_level = 4 },
   ]
 }
 
@@ -29,13 +27,13 @@ module "security_policy" {
   json_parsing                         = "STANDARD"
 
   pre_configured_rules = {
-    for idx, rule in local.waf_rules : split(",", rule)[0] => {
-      target_rule_set         = rule
+    for idx, rule in local.waf_rules : split(",", rule.target_rule_set)[0] => {
+      target_rule_set         = rule.target_rule_set
       priority                = idx
       action                  = "deny(502)"
-      sensitivity_level       = 4
+      sensitivity_level       = rule.sensitivity_level
       preview                 = true
-      exclude_target_rule_ids = compact([for ignored in var.ignored_rules : (endswith(ignored, split(",", rule)[0]) ? ignored : "")])
+      exclude_target_rule_ids = compact([for ignored in var.ignored_rules : (endswith(ignored, split(",", rule.target_rule_set)[0]) ? ignored : "")])
     }
   }
 }
