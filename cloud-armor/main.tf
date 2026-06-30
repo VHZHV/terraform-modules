d3d8d1a76ffd0ce8c230d8d6c0448a6a673b4cfd
Adam Bennett <adam@hozah.com>
2026-06-30 18:17:32 +0100

    [HPS-761] Cloud Armor security policy on external load balancer
    Signed-off-by: Adam Bennett <adam@hozah.com>

--- cloud-armor/main.tf
+++ cloud-armor/main.tf
@@ -1,7 +1,7 @@
 locals {
   waf_rules = [
     { target_rule_set = "lfi-v422-stable", sensitivity_level = 4, preview = true },
-    { target_rule_set = "rfi-v422-stable", sensitivity_level = 4, preview = true },
+    { target_rule_set = "rfi-v422-stable", sensitivity_level = 1, preview = true },
     { target_rule_set = "scannerdetection-v422-stable", sensitivity_level = 4, preview = false },
     { target_rule_set = "sessionfixation-v422-stable", sensitivity_level = 4, preview = true },
     { target_rule_set = "java-v422-stable", sensitivity_level = 4, preview = true },
