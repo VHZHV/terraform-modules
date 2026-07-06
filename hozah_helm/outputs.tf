0b427e67ec91abbf8181763456b7d883894587ae
Adam Bennett <adam@hozah.com>
2026-07-06 18:16:01 +0100

    Dependencies not managed by dependabot
    Signed-off-by: Adam Bennett <adam@hozah.com>

--- hozah_helm/outputs.tf
+++ hozah_helm/outputs.tf
@@ -1,6 +1,6 @@
 # This mainly exists to keep everyone up to date with kubernetes-commons, as dependabot can't really look at helm in terraform
 output "version" {
-  value = "1.38.0"
+  value = "1.39.0"
 }
 output "repository" {
   value = "oci://europe-docker.pkg.dev"
--- opentelemetry-operator/main.tf
+++ opentelemetry-operator/main.tf
@@ -2,7 +2,7 @@ resource "helm_release" "k8s_operator_for_opentelemetry" {
   name             = "opentelemetry-operator"
   repository       = "https://open-telemetry.github.io/opentelemetry-helm-charts"
   chart            = "opentelemetry-operator"
-  version          = "0.115.0"
+  version          = "0.118.0"
   create_namespace = true
   # Need the contrib distribution for googleclientauth / otlphttp.
   set = [{
