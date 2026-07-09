9dd0b52ff378f093e7eac3787833f2a16875ce4e
Adam Bennett <adam@hozah.com>
2026-07-09 10:57:39 +0100

    Downgrade workload identity: https://github.com/terraform-google-modules/terraform-google-kubernetes-engine/issues/2616
    Signed-off-by: Adam Bennett <adam@hozah.com>

--- workload_identity/main.tf
+++ workload_identity/main.tf
@@ -1,6 +1,6 @@
 module "workload_identity" {
   source  = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
-  version = "44.3.0"
+  version = "44.2.0"
 
   name = "${var.region_short}-${substr(var.service_name, 0, 20)}-${var.environment_name}"
 
