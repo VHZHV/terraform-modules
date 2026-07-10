1f50a70220da159438cb01be36937a2f138a018b
Adam Bennett <adam@hozah.com>
2026-07-10 10:44:52 +0100

    [HPS-760] DR failover test: europe-west2 → europe-west1
    Signed-off-by: Adam Bennett <adam@hozah.com>

---
+++ service_accounts/versions.tf
@@ -0,0 +1,9 @@
+terraform {
+  required_version = ">= 0.13"
+  required_providers {
+    string-functions = {
+      source  = "random-things/string-functions"
+      version = "~> 0.0"
+    }
+  }
+}
