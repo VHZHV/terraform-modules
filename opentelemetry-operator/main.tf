de68ff7ef08995c4b8cc170a7c4949adae5bd1cc
Adam Bennett <54963794+adben002@users.noreply.github.com>
2026-07-20 10:58:31 +0100

    Non-managed dependency upgrades
    operator 0.119.0 -> 0.120.0

--- opentelemetry-operator/main.tf
+++ opentelemetry-operator/main.tf
@@ -2,7 +2,7 @@ resource "helm_release" "k8s_operator_for_opentelemetry" {
   name             = "opentelemetry-operator"
   repository       = "https://open-telemetry.github.io/opentelemetry-helm-charts"
   chart            = "opentelemetry-operator"
-  version          = "0.119.0"
+  version          = "0.120.0"
   create_namespace = true
   # Need the contrib distribution for googleclientauth / otlphttp.
   set = [{
