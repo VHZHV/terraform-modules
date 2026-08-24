d14d97088ed95bf91a544a28a757dbda2c54bda8
sem <adam@hozah.com>
2026-08-24 10:26:29 +0100

    google_sql_user deletion_policy
    Signed-off-by: sem <adam@hozah.com>

--- db/main.tf
+++ db/main.tf
@@ -3,6 +3,8 @@ resource "google_sql_user" "admin_user" {
   instance            = var.cloud_sql_instance_name
   password_wo         = var.password
   password_wo_version = 1
+
+  deletion_policy = var.deletable ? "DELETE" : "ABANDON"
 }
 
 resource "google_sql_database" "main_database" {
