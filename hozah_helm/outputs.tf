a221e32108f86e8593cec7131df4fad688a00211
Adam Bennett <54963794+adben002@users.noreply.github.com>
2026-08-31 11:59:38 +0100

    Update outputs.tf

--- hozah_helm/outputs.tf
+++ hozah_helm/outputs.tf
@@ -1,6 +1,6 @@
 # This mainly exists to keep everyone up to date with kubernetes-commons, as dependabot can't really look at helm in terraform
 output "version" {
-  value = "1.49.0"
+  value = "1.50.0"
 }
 output "repository" {
   value = "oci://europe-docker.pkg.dev"
